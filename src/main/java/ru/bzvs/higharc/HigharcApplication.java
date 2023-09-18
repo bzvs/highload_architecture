95a8c356dc3db4d0eba5d09b1f29e60c02a2b1d6
vbezzubenko@mmtr.ru <vbezzubenko@mmtr.ru>
2023-09-18 15:33:07 +0300

    initial commit

---
+++ src/main/java/ru/bzvs/higharc/HigharcApplication.java
@@ -0,0 +1,13 @@
+package ru.bzvs.higharc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HigharcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HigharcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/bzvs/higharc/HigharcApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.bzvs.higharc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HigharcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
